5138da0954ee7450af99fc5edcaa05928e83db40
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-11-23 22:48:43 -0800

    added weather data

--- routes/routes.js
+++ routes/routes.js
@@ -1,22 +1,22 @@
 'use strict';
 
-var Elist = require('..models.elist');
+var request = require('superagent');
 
 module.exports = function(app){
-  app.post('/', function(req, res){
-  var name = req.body.name;
-  var email = req.body.email;
-  });
 
-Elist.findOne({email: email}, function(err, user){
-  if(err) return res.status(404).send('server error');
-  if(!user) {
-    var newMem = new Elist(req.body);
-    newMem.name = req.body.name;
-    newMem.email = req.body.email;
-    newMem.save(function(err){
-    res.send(newMem.email "Thank you" + newMem.name + "for signing up for my email");
+  app.post('/', function(req, res){
+  var url = "http://api.wunderground.com/api/" + '828e3a84bb61c1a2' + "/geolookup/conditions/q/" + 'WA/Seattle'+ ".json";
+
+    request
+    .get(url)
+    .end(function (err, urlData){
+      var parsedBody = JSON.parse(urlData.text);
+      if(err) throw err;
+      var cond = parsedBody.current_observation.weather;
+      var icon = parsedBody.current_observation.icon_url;
+      var temp = parsedBody.current_observation.temp_f;
+
+    res.send({location: "Seattle", current_temps: temp, conditions: cond, outside:icon });
   });
 });
-    }
-});
+};
