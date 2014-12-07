4e841f06eeccd9d66373ffc26e77b95bce8ebd95
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2014-12-06 23:23:04 -0800

    more styling on front page

--- public/js/flow.js
+++ public/js/flow.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+  (function(doc, script) {
+    var js, id;
+    fjs = doc.getElementsByTagName(script)[0],
+    frag = doc.createDocumentFragment(),
+    add = function(url, id) {
+      if (doc.getElementById(id)) {return;}
+        js = doc.createElement(script);
+        js.src = url;
+        id && (js.id = id);
+        frag.appendChild( js );
+      }
+
+      // Google+ button
+      add('http://apis.google.com/js/plusone.js');
+      // Facebook SDK
+      add('//connect.facebook.net/en_US/all.js#xfbml=1&appId=200103733347528', 'facebook-jssdk');
+      // Twitter SDK
+      add('//platform.twitter.com/widgets.js');
+
+      fjs.parentNode.insertBefore(frag, fjs);
+    }(document, 'script'));
 
   $.post('/', function(urlData){
     $('#temp').append('Temperature: ' + urlData.current_temps);
