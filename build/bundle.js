87825a2930cc87f699b113e133a96f22d58b1adf
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2015-01-10 16:38:41 -0800

    tweak

--- build/bundle.js
+++ build/bundle.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 $(document).ready(function() {
 
   $.post('/', function(urlData){
-    $('#temp').append('Temperature: ' + urlData.current_temps);
+    $('#temp').append('Temperature: ' + urlData.current_temps);    
     $('#cond').append('Conditions: ' + urlData.conditions);
     $('#weather').append('<img src="'+urlData.outside+'">');
   });
