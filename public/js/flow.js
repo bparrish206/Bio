0b78d9000bb5070816289be65f006879fc3c204c
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2015-01-10 17:38:15 -0800

    tweak

--- public/js/flow.js
+++ public/js/flow.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
   $.post('/', function(urlData){
-    $('#temp').append('Temperature: ' + urlData.current_temps +'&deg;');   
+    $('#temp').append('Temperature: ' + urlData.current_temps +'&deg;' +'F');   
     $('#cond').append('Conditions: ' + urlData.conditions);
     $('#weather').append('<img src="'+urlData.outside+'">');
   });
