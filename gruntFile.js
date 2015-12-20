55f7711cd6f25695255ac5b251e1c3ab8957185f
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2015-12-19 18:08:28 -0800

    redid front page

--- gruntFile.js
+++ gruntFile.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-module.exports = function(grunt){
+module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
