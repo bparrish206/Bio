dcbc1c978e7e017ad68ef98636d07b3a884be315
bparrish206 <brentparrish@Rosias-MacBook-Pro.local>
2016-08-07 23:56:57 -0700

    small style changes

--- public/js/flow.js
+++ public/js/flow.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
   $.post('/', function(urlData){
-    $('#temp').append('Temperature: ' + urlData.current_temps +'&deg;' +'F');
+    $('#temp').append('Temp: ' + urlData.current_temps +'&deg;' +'F');
     $('#cond').append('Conditions: ' + urlData.conditions);
     $('#weather').append('<lil> <img src="'+urlData.outside+'"> </li>');
   });
